708c88fb207d7fba73e3b1abaf3dc3026a6b15a1
alanyukeroo@gmail.com <alanyukeroo@gmail.com>
2021-09-17 23:10:34 +0700

    update

--- anonymous-function.go
+++ anonymous-function.go
@@ -1,7 +1,21 @@
 package main
 
-type BlackList func(string)
+import "fmt"
+
+type BlackList func(string) bool
+
+func registerUser(name string, blacklist BlackList) {
+	if blacklist(name) {
+		fmt.Println("You are blocked", name)
+	} else {
+		fmt.Println("Welcome!", name)
+	}
+}
 
 func main() {
+	blacklist := func(name string) bool {
+		return name == "admin"
+	}
 
+	registerUser("Alan", blacklist)
 }
